a4a96d919c329be1181aa386fd340170c6d88767
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-30 16:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 fetch("https://jsonplaceholder.typicode.com/todos/1")
 .then(data => data.json())
-.then(dataobj => console.log(data))
+.then(dataobj => console.log(dataobj))
 .catch(()=>{console.log("err")})
 
